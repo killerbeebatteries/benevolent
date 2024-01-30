1a0bbb19b83f0920eb177fafb374cc3e9221dcb5
ops <somewhere@example.com>
2024-01-30 19:39:03 +0930

    *dance* working weather thanks to zek and chatGPT

--- sandpit/weather.go
+++ sandpit/weather.go
@@ -1,84 +1,93 @@
 package main
 
 import (
-	"encoding/xml"
-	"fmt"
-	"log"
-	"strings"
+  "encoding/xml"
+  "fmt"
+  "log"
+  "strings"
   "io/ioutil"
   "os"
 
   "github.com/jlaffaye/ftp"
 )
-
-// Product represents the root structure of the XML data
+// Product was generated 2024-01-30 19:32:16 using Zek: https://github.com/miku/zek
 type Product struct {
-	XMLName xml.Name `xml:"product"`
-	AMOC    AMOC     `xml:"amoc"`
-	Forecast Forecast `xml:"forecast"`
-}
+  XMLName                   xml.Name `xml:"product"`
+  Text                      string   `xml:",chardata"`
+  Version                   string   `xml:"version,attr"`
+  Xsi                       string   `xml:"xsi,attr"`
+  NoNamespaceSchemaLocation string   `xml:"noNamespaceSchemaLocation,attr"`
+  Amoc                      struct {
+    Text   string `xml:",chardata"`
+    Source struct {
+      Text       string `xml:",chardata"`
+      Sender     string `xml:"sender"`     // Australian Government Bur...
+      Region     string `xml:"region"`     // New South Wales
+      Office     string `xml:"office"`     // NSWRO
+      Copyright  string `xml:"copyright"`  // http://www.bom.gov.au/oth...
+      Disclaimer string `xml:"disclaimer"` // http://www.bom.gov.au/oth...
+    } `xml:"source"`
+    Identifier     string `xml:"identifier"`     // IDN11060
+    IssueTimeUtc   string `xml:"issue-time-utc"` // 2024-01-30T04:50:00Z
+    IssueTimeLocal struct {
+      Text string `xml:",chardata"` // 2024-01-30T15:50:00+11:00...
+      Tz   string `xml:"tz,attr"`
+    } `xml:"issue-time-local"`
+    SentTime             string `xml:"sent-time"`   // 2024-01-30T04:50:06Z
+    ExpiryTime           string `xml:"expiry-time"` // 2024-01-31T04:50:00Z
+    ValidityBgnTimeLocal struct {
+      Text string `xml:",chardata"` // 2024-01-30T17:00:00+11:00...
+      Tz   string `xml:"tz,attr"`
+    } `xml:"validity-bgn-time-local"`
+    ValidityEndTimeLocal struct {
+      Text string `xml:",chardata"` // 2024-02-06T23:59:59+11:00...
+      Tz   string `xml:"tz,attr"`
+    } `xml:"validity-end-time-local"`
+    NextRoutineIssueTimeUtc   string `xml:"next-routine-issue-time-utc"` // 2024-01-30T17:25:00Z
+    NextRoutineIssueTimeLocal struct {
+      Text string `xml:",chardata"` // 2024-01-31T04:25:00+11:00...
+      Tz   string `xml:"tz,attr"`
+    } `xml:"next-routine-issue-time-local"`
+    Status      string `xml:"status"`       // O
+    Service     string `xml:"service"`      // WSP
+    SubService  string `xml:"sub-service"`  // FPR
+    ProductType string `xml:"product-type"` // F
+    Phase       string `xml:"phase"`        // NEW
+  } `xml:"amoc"`
+  Forecast struct {
+    Text string `xml:",chardata"`
+    Area []struct {
+      Text           string `xml:",chardata"`
+      Aac            string `xml:"aac,attr"`
+      Description    string `xml:"description,attr"`
+      Type           string `xml:"type,attr"`
+      ParentAac      string `xml:"parent-aac,attr"`
+      ForecastPeriod []struct {
+        Chardata       string `xml:",chardata"`
+        Index          string `xml:"index,attr"`
+        StartTimeLocal string `xml:"start-time-local,attr"`
+        EndTimeLocal   string `xml:"end-time-local,attr"`
+        StartTimeUtc   string `xml:"start-time-utc,attr"`
+        EndTimeUtc     string `xml:"end-time-utc,attr"`
+        Element        []struct {
+          Text  string `xml:",chardata"` // 17, 11, 0 to 4 mm, 22, 26...
+          Type  string `xml:"type,attr"`
+          Units string `xml:"units,attr"`
+        } `xml:"element"`
+        Text []struct {
+          Text string `xml:",chardata"` // Possible shower., 40%!,(MISSING) Sh...
+          Type string `xml:"type,attr"`
+        } `xml:"text"`
+      } `xml:"forecast-period"`
+    } `xml:"area"`
+  } `xml:"forecast"`
+} 
 
-// AMOC represents the AMOC section of the XML data
-type AMOC struct {
-	Source                 Source `xml:"source"`
-	Identifier             string `xml:"identifier"`
-	IssueTimeUTC           string `xml:"issue-time-utc"`
-	IssueTimeLocal         string `xml:"issue-time-local"`
-	SentTime               string `xml:"sent-time"`
-	ExpiryTime             string `xml:"expiry-time"`
-	ValidityBgnTimeLocal   string `xml:"validity-bgn-time-local"`
-	ValidityEndTimeLocal   string `xml:"validity-end-time-local"`
-	NextRoutineIssueTimeUTC string `xml:"next-routine-issue-time-utc"`
-	NextRoutineIssueTimeLocal string `xml:"next-routine-issue-time-local"`
-	Status                 string `xml:"status"`
-	Service                string `xml:"service"`
-	SubService             string `xml:"sub-service"`
-	ProductType            string `xml:"product-type"`
-	Phase                  string `xml:"phase"`
-}
-
-// Source represents the source section of the XML data
-type Source struct {
-	Sender     string `xml:"sender"`
-	Region     string `xml:"region"`
-	Office     string `xml:"office"`
-	Copyright  string `xml:"copyright"`
-	Disclaimer string `xml:"disclaimer"`
-}
-
-// Forecast represents the forecast section of the XML data
-type Forecast struct {
-	Areas []Area `xml:"area"`
-}
-
-// Area represents a forecast area
-type Area struct {
-	AAC         string          `xml:"aac,attr"`
-	Description string          `xml:"description,attr"`
-	Type        string          `xml:"type,attr"`
-	ParentAAC   string          `xml:"parent-aac,attr,omitempty"`
-	Periods     []ForecastPeriod `xml:"forecast-period"`
-}
-
-// ForecastPeriod represents details of a forecast period
-type ForecastPeriod struct {
-	Index                 int     `xml:"index,attr"`
-	StartTimeLocal        string  `xml:"start-time-local,attr"`
-	EndTimeLocal          string  `xml:"end-time-local,attr"`
-	StartTimeUTC          string  `xml:"start-time-utc,attr"`
-	EndTimeUTC            string  `xml:"end-time-utc,attr"`
-	IconCode              int     `xml:"element[type='forecast_icon_code']"`
-	Precis                string  `xml:"text[type='precis']"`
-	ProbabilityOfPrecip   string  `xml:"text[type='probability_of_precipitation']"`
-	PrecipitationRange    string  `xml:"element[type='precipitation_range'],omitempty"`
-	TemperatureMinimum    float64 `xml:"element[type='air_temperature_minimum'],attr,omitempty"`
-	TemperatureMaximum    float64 `xml:"element[type='air_temperature_maximum'],attr,omitempty"`
-}
 
 func parseXML(xmlData string) (*Product, error) {
-	var product Product
-	err := xml.NewDecoder(strings.NewReader(xmlData)).Decode(&product)
-	return &product, err
+  var product Product
+  err := xml.NewDecoder(strings.NewReader(xmlData)).Decode(&product)
+  return &product, err
 }
 
 func getWeather() (string, error) {
@@ -87,6 +96,8 @@ func getWeather() (string, error) {
   ftpUser := os.Getenv("FTP_USER")
   ftpPassword := os.Getenv("FTP_PASSWORD")
   ftpFilePath := os.Getenv("FTP_FILE_PATH") + "/" + os.Getenv("FTP_FILE_NAME")
+
+  fmt.Println("Retrieving data from " + ftpServer + ftpFilePath + "...")
 
   // Connect to FTP server
   conn, err := ftp.Dial(fmt.Sprintf("%s:%d", ftpServer, 21))
@@ -118,34 +129,49 @@ func getWeather() (string, error) {
 }
 
 func main() {
-	// Replace the following line with code to fetch the XML data from the FTP server
+  // Replace the following line with code to fetch the XML data from the FTP server
   xmlData, err := getWeather()
   if err != nil {
      fmt.Println(err)
   }
 
-	// Parse XML data
-	parsedData, err := parseXML(xmlData)
-	if err != nil {
-		log.Fatal(err)
-	}
+  // Parse XML data
+  product, err := parseXML(xmlData)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	// Access parsed data
-	fmt.Printf("Identifier: %s\n", parsedData.AMOC.Identifier)
-	fmt.Printf("Issue Time (UTC): %s\n", parsedData.AMOC.IssueTimeUTC)
-	fmt.Printf("Validity Begin Time (Local): %s\n", parsedData.AMOC.ValidityBgnTimeLocal)
+  // Access parsed data
+  fmt.Printf("Product Version: %s\n", product.Version)
+  fmt.Printf("Product Xsi: %s\n", product.Xsi)
+  fmt.Printf("Product NoNamespaceSchemaLocation: %s\n", product.NoNamespaceSchemaLocation)
 
-	for _, area := range parsedData.Forecast.Areas {
-		fmt.Printf("\nArea: %s (%s)\n", area.Description, area.Type)
-		for _, period := range area.Periods {
-			fmt.Printf("  Period: %s to %s\n", period.StartTimeLocal, period.EndTimeLocal)
-			fmt.Printf("  Precis: %s\n", period.Precis)
-			fmt.Printf("  Probability of Precipitation: %s\n", period.ProbabilityOfPrecip)
-			fmt.Printf("  Temperature Range: %.2f°C to %.2f°C\n", period.TemperatureMinimum, period.TemperatureMaximum)
-			if period.PrecipitationRange != "" {
-				fmt.Printf("  Precipitation Range: %s\n", period.PrecipitationRange)
-			}
-		}
-	}
+  fmt.Printf("AMOC Identifier: %s\n", product.Amoc.Identifier)
+  fmt.Printf("AMOC Issue Time (UTC): %s\n", product.Amoc.IssueTimeUtc)
+  fmt.Printf("AMOC Issue Time (Local): %s %s\n", product.Amoc.IssueTimeLocal.Text, product.Amoc.IssueTimeLocal.Tz)
+  fmt.Printf("AMOC Sent Time: %s\n", product.Amoc.SentTime)
+  fmt.Printf("AMOC Expiry Time: %s\n", product.Amoc.ExpiryTime)
+  fmt.Printf("AMOC Validity Begin Time (Local): %s %s\n", product.Amoc.ValidityBgnTimeLocal.Text, product.Amoc.ValidityBgnTimeLocal.Tz)
+  fmt.Printf("AMOC Validity End Time (Local): %s %s\n", product.Amoc.ValidityEndTimeLocal.Text, product.Amoc.ValidityEndTimeLocal.Tz)
+  fmt.Printf("AMOC Next Routine Issue Time (UTC): %s\n", product.Amoc.NextRoutineIssueTimeUtc)
+  fmt.Printf("AMOC Next Routine Issue Time (Local): %s %s\n", product.Amoc.NextRoutineIssueTimeLocal.Text, product.Amoc.NextRoutineIssueTimeLocal.Tz)
+  fmt.Printf("AMOC Status: %s\n", product.Amoc.Status)
+  fmt.Printf("AMOC Service: %s\n", product.Amoc.Service)
+  fmt.Printf("AMOC SubService: %s\n", product.Amoc.SubService)
+  fmt.Printf("AMOC Product Type: %s\n", product.Amoc.ProductType)
+  fmt.Printf("AMOC Phase: %s\n", product.Amoc.Phase)
+
+  for _, area := range product.Forecast.Area {
+    fmt.Printf("\nArea: %s (%s)\n", area.Description, area.Type)
+    for _, period := range area.ForecastPeriod {
+        fmt.Printf("  Period: %s to %s\n", period.StartTimeLocal, period.EndTimeLocal)
+        for _, elem := range period.Element {
+            fmt.Printf("    Element Type: %s, Value: %s %s\n", elem.Type, elem.Text, elem.Units)
+        }
+        for _, text := range period.Text {
+            fmt.Printf("    Text Type: %s, Value: %s\n", text.Type, text.Text)
+        }
+    }
+  }
 }
 
